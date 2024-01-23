f262bda5f0b525b635824162f1f171c6fb57b8ef
SimoneCappadona <simocappa95@gmail.com>
2024-01-23 17:34:58 +0100

    add structure for team

--- js/main.js
+++ js/main.js
@@ -32,10 +32,37 @@ const team = [
   },
 ];
 
-// Team console log info
+// Stampa in console le info del team
 for (let i = 0; i < team.length; i++) {
   for (let teamEl in team[i]) {
     console.log(teamEl + " : " + team[i][teamEl]);
   }
   console.log("");
+}
+// Stampa in pagina le info del team
+const container = document.getElementById("container");
+for (let i = 0; i < team.length; i++) {
+  // ContainerCard <div>
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("cardContainer");
+  // card <div>
+  const card = document.createElement("div");
+  card.classList.add("card");
+  cardContainer.appendChild(card);
+  // Img
+  const image = document.createElement("img");
+  image.src = team[i].image;
+  card.appendChild(image);
+  // Name Team
+  const name = document.createElement("div");
+  name.classList.add("name");
+  name.textContent = team[i].nabe;
+  card.appendChild(name);
+  // Role Team
+  const role = document.createElement("div");
+  role.classList.add("role");
+  role.textContent = team[i].role;
+  card.appendChild(role);
+  // Append to CardContainer
+  container.appendChild(cardContainer);
 }
