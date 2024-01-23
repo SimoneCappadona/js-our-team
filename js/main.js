5f9ff46dce8db1b3dcf64eb0c5b943b7dd32a712
SimoneCappadona <simocappa95@gmail.com>
2024-01-23 15:39:00 +0100

    console log info team

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Array contenente i membri del team
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "img/wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "img/angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    image: "img/walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "img/angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "img/scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "img/barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+// Team console log info
+for (let i = 0; i < team.length; i++) {
+  for (let teamEl in team[i]) {
+    console.log(teamEl + " : " + team[i][teamEl]);
+  }
+  console.log("");
+}
